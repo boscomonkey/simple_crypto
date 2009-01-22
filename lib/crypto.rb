41c3e5aa8faa5c661ead04ed3df63debb2c224e7
Bosco So <git@boscoso.com>
2009-01-21 21:38:12 -0800

    Changed usage: Before - create crypto with salt (key is predefined) After  - create crypto with key (pass salt to encrypt & decrypt)

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,51 +1,60 @@
 require 'openssl'
 require 'digest/sha2'
 
-# Code from http://tinyurl.com/43zvt4 modified so that a Crypto instance is
-# created with a salt and that instance is used to encrypt and decrypt. The
-# salt is unique for each encryption & decryption session.
+# This code is derived from Robert Sosinski's "Simple and Restful
+# Account Recovery for Ruby on Rails" - http://tinyurl.com/43zvt4
 #
-# A salt can be created with Internaut Design's easy algorithm:
-#   DateTime.now.hash.abs.to_s(36)
+# Create a new SimpleCrypto object with a key:
+#       crypto = SimpleCrypto.new 'some random key'
 #
-class Crypto
-  KEY = "3efb989671ff4f48e22152660a49b18c"
-  
+# Encrypt a string (i.e., password) into a hex string
+#       enc_hex = crypto.encrypt 'a secret password'
+#       # => "71a7343283d61656973ba8923bde22e92b6ed2078da390ab422d8f2377836173"
+#
+# Decrypt said hex string back into the original string
+#       orig = crypto.decrypt enc_hex
+#
+# Add a salt so that the same password doesn't encrypt to the same hex string
+#       salt = SimpleCrypto.new_salt
+#       hex2 = crypto.encrypt 'a secret password', salt
+#       # => "b14672abb69d077fe43cf57c47b54b448088c782fec4198c2c3548aa8d587b73"
+#
+# Pass the salt to decrypt to get the same string back
+#       orig = crypto.decrypt hex2, salt
+# 
+class SimpleCrypto
   def self.new_salt
-    DateTime.now.hash.abs.to_s(36)
+    # from Internaut Design's blog
+    Time.now.hash.abs.to_s(36)
   end
   
-  def initialize(salt='')
-    @salt = salt
+  def initialize(key)
+    @key = key
   end
   
-  def encrypt(plain_text)
-    crypto = start(:encrypt)
+  def encrypt(plain_text, salt='')
+    crypto = start(:encrypt, salt)
     
     cipher_text = crypto.update(plain_text)
     cipher_text << crypto.final
     
     cipher_hex = cipher_text.unpack("H*").join
-    
-    return cipher_hex
   end
   
-  def decrypt(cipher_hex)
-    crypto = start(:decrypt)
+  def decrypt(cipher_hex, salt='')
+    crypto = start(:decrypt, salt)
     
     cipher_text = cipher_hex.gsub(/(..)/){|h| h.hex.chr}
     
     plain_text = crypto.update(cipher_text)
     plain_text << crypto.final
-    
-    return plain_text
   end
   
   protected
   
-  def start(mode)
+  def start(mode, salt)
     crypto = OpenSSL::Cipher::Cipher.new('aes-256-ecb').send(mode)
-    crypto.key = Digest::SHA256.hexdigest(KEY + @salt)
-    return crypto
+    crypto.key = Digest::SHA256.hexdigest(@key + salt)
+    crypto
   end
 end
